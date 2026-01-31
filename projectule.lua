e78116a3a0ec769eb6d5705d1092c34889ace395
ColinDScahffner <schacd01@pfw.edu>
2026-01-30 21:51:42 -0500

    Changed implementation of projectile

--- projectule.lua
+++ projectule.lua
@@ -1,17 +1,21 @@
-require("middleclass")
+local class = require("middleclass")
 
-projectile = {
-	image = "",
-	x = 0,
-	load = function()
-		background.image = love.graphics.newImage("Assets/background.png")
-	end,
-	update = function()
+Projectile = class('Projectile')
+
+function Projectile:initialize(x, y, image)
+	self.x = x
+	self.y = y
+	self.image = image
+end
+
+function Projectile:load()
+	self.image = love.graphics.newImage("Assets/background.png")
+end
+
+function Projectile:update()
 		
-	end,
-	draw = function()
-		love.graphics.draw(background.image, background.x, 0, 0, 1, 1)
-	end
-}
+end
 
-return projectile
+function Projectile:draw()
+	love.graphics.draw(background.image, background.x, 0, 0, 1, 1)
+end
