c3a4204ccbae4946d042133e1cefe70777461855
ColinDScahffner <schacd01@pfw.edu>
2026-01-30 21:13:58 -0500

    Player physics update

--- main.lua
+++ main.lua
@@ -1,20 +1,29 @@
 windowW = 1280
 windowH = 720
+groundH = 20
 player = require("player")
+Camera = require "CameraMgr".newManager()
 
 function love.load()
 	love.window.setMode(windowW, windowH)
 	player.load()
 end
 
-function love.update()
-
+function love.update(dt)
+	player.update(dt)
+	Camera.setCoords(player.x, 360)
+	Camera.update(dt)
 end
 
 function love.draw()
+	Camera.attach()
+
 	love.graphics.setColor(0,0,0)
 	love.graphics.rectangle("fill", 0, 0, 500, 500)
 	love.graphics.setColor(1,1,1)
 	love.graphics.print("hello!", 250, 250)
+	rectLine(0, 0, windowW, windowH, {1,1,1})
 	player.draw()
+
+	Camera.detach()
 end
--- player.lua
+++ player.lua
@@ -1,19 +1,53 @@
 require("misc")
 
+terminalV = 300
+gravityV = 200
+maxXV = 500
+slideV = 600
+accV = 1300
+
 player = {
 	x = 0,
+	dx = 0,
 	y = 0,
+	dy = 0,
 	w = 60,
 	h = 100,
+	airborne = true,
+	pressedAOrD = false,
 	load = function()
 		player.x = 0
-		player.y = windowH - player.h
+		player.y = 0--windowH - player.h
 	end,
-	update = function()
+	update = function(dt)
+		player.pressedAOrD = false
+		if player.airborne then
+			player.y = player.y + player.dy * dt
+			player.dy = player.dy + dt*gravityV
+			if player.y + player.h >= windowH - groundH then
+				player.airborne = false
+			end
+		end
 
+		if love.keyboard.isDown("a") then
+			player.dx = math.min(0, player.dx - dt*accV)
+			player.pressedAOrD = true
+		end
+		if love.keyboard.isDown("d") then
+			player.dx = math.max(0, player.dx + dt*accV)
+			player.pressedAOrD = true
+		end
+		player.x = player.x + player.dx * dt
+
+		local slideX = player.pressedAOrD and 1 or 2
+		player.x = mid(0, player.x, windowW-player.w)
+		player.dx = mid(-maxXV, mid(player.dx + slideV*slideX*dt, 0, player.dx - slideV*slideX*dt), maxXV)
+		player.y = mid(0, player.y, windowH-groundH-player.h)
+		player.dy = math.min(terminalV, player.dy)
 	end,
 	draw = function()
 		rect(player.x, player.y, player.w, player.h, {1,1,1})
+		text(player.dx, player.x, player.y - 30, 12, {1,1,1})
 	end
 }
 
