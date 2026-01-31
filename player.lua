aadf56cdf7c67d2f8ac2372395f6c2efb82b6eb0
ColinDScahffner <schacd01@pfw.edu>
2026-01-30 21:44:06 -0500

    Jumping and enemy file

--- player.lua
+++ player.lua
@@ -1,10 +1,11 @@
 require("misc")
 
-terminalV = 300
-gravityV = 200
+terminalV = 400
+gravityV = 700
 maxXV = 500
 slideV = 600
 accV = 1300
+jumpV = -500
 
 player = {
 	x = 0,
@@ -22,11 +23,16 @@ player = {
 	update = function(dt)
 		player.pressedAOrD = false
 		if player.airborne then
-			player.y = player.y + player.dy * dt
-			player.dy = player.dy + dt*gravityV
+			local down = love.keyboard.isDown("s")
+			local up = love.keyboard.isDown("space")
+			player.dy = down and math.max(player.dy, 0) or player.dy
+			player.dy = player.dy + dt*gravityV*(down and 2 or 1)*(up and .5 or 1)
 			if player.y + player.h >= windowH - groundH then
 				player.airborne = false
 			end
+		elseif love.keyboard.isDown("space") then
+			player.airborne = true
+			player.dy = jumpV
 		end
 
 		if love.keyboard.isDown("a") then
@@ -38,12 +44,12 @@ player = {
 			player.pressedAOrD = true
 		end
 		player.x = player.x + player.dx * dt
+		player.y = player.y + player.dy * dt
 
 		local slideX = player.pressedAOrD and 1 or 2
-		player.x = mid(0, player.x, windowW-player.w)
 		player.dx = mid(-maxXV, mid(player.dx + slideV*slideX*dt, 0, player.dx - slideV*slideX*dt), maxXV)
 		player.y = mid(0, player.y, windowH-groundH-player.h)
-		player.dy = math.min(terminalV, player.dy)
+		--player.dy = math.min(terminalV, player.dy)
 	end,
 	draw = function()
 		rect(player.x, player.y, player.w, player.h, {1,1,1})
